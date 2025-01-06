f5d61b746b75f94b63c3f3386e37a975caedf4b6
Kevin Meyer <kevin.meyer@fulfil.com>
2025-01-06 18:56:34 -0500

    Same tree

--- 0100_same_tree.py
+++ 0100_same_tree.py
@@ -1,1 +1,15 @@
-#!/
+#!/usr/bin/env python3
+
+from util.data_structures import TreeNode
+from typing import Optional
+
+class Solution:
+    # I have another version of this 
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
